dda835bf42cad6e8fbe609893c4bcca4fcb426bf
Benjamin Thorand <thorand.benjamin@gmail.com>
2018-12-30 22:52:20 +0100

    feat: draw the map and highlight cities

--- D3js/Maps/Zoomable/app.js
+++ D3js/Maps/Zoomable/app.js
@@ -21,12 +21,30 @@ var dims = {
         "#006d2c",
         "#00441b"
     ])
-}
+};
+
+var projection = d3.geoAlbersUsa()
+    .translate([0,0]);
+var path=d3.geoPath(projection);
+var r_scale=d3.scaleLinear()
+    .domain([0,8000000])
+    .range([5,20]);
 
 var svg =  d3.select('#chart')
             .append('svg')
             .attr('width', dims.width)
             .attr('height', dims.height);
+
+var map = svg.append('g')
+    .attr('id', 'map')
+    .attr('cursor', 'pointer');
+
+map.append('rect')
+    .attr('x',0)
+    .attr('y',0)
+    .attr('width', dims.width)
+    .attr('height', dims.height)
+    .attr('opacity', 0);
 
 d3.json('zombie-attacks.json').then(function(zombie_data,error){
     dims.color.domain([
@@ -47,5 +65,46 @@ d3.json('zombie-attacks.json').then(function(zombie_data,error){
                us_data.features[us_i].properties.num=parseFloat(z_e.num);
            })
        })
+
+        console.log(us_data);
+
+       map.selectAll('path')
+           .data(us_data.features)
+           .enter()
+           .append('path')
+           .attr('d', path)
+           .attr('fill', function(d){
+               var num = d.properties.num;
+               return num ? dims.color(num) : '#fff';
+           })
+           .attr('stroke', '#fff')
+           .attr('stroke-width', 2);
+
+       draw_cities();
     });
 });
+
+function draw_cities(){
+    d3.json("us-cities.json").then(function (city_data) {
+        map.selectAll("circle")
+            .data(city_data)
+            .enter()
+            .append("circle")
+            .style("fill", "#9d1c3c")
+            .style("opacity", 0.8)
+            .attr("cx",function(d){
+                return projection([d.lon,d.lat])[0];
+            })
+            .attr("cy",function(d){
+                return projection([d.lon,d.lat])[1];
+            })
+            .attr("r",function(d){
+                return r_scale(d.population);
+            })
+            .append("title")
+            .text(function(d){
+                return d.city;
+            });
+
+    });
+}
