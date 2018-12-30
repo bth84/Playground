46ba602a6020918083e529298ab7020c2519515f
Benjamin Thorand <thorand.benjamin@gmail.com>
2018-12-30 22:37:49 +0100

    feat: loading data
    loads data using d3.json

--- D3js/Maps/Zoomable/app.js
+++ D3js/Maps/Zoomable/app.js
@@ -27,3 +27,25 @@ var svg =  d3.select('#chart')
             .append('svg')
             .attr('width', dims.width)
             .attr('height', dims.height);
+
+d3.json('zombie-attacks.json').then(function(zombie_data,error){
+    dims.color.domain([
+        d3.min(zombie_data, function(d){
+            return d.num;
+        }),
+        d3.max(zombie_data, function(d){
+            return d.num;
+        })
+    ]);
+
+    d3.json('us.json').then(function(us_data, error){
+       us_data.features.forEach(function(us_e, us_i){
+           zombie_data.forEach(function(z_e, z_i){
+               if(us_e.properties.name !== z_e.state){
+                   return null;
+               }
+               us_data.features[us_i].properties.num=parseFloat(z_e.num);
+           })
+       })
+    });
+});
