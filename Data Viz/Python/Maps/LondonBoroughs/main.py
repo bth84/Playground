1874a65ae938d7dccdf45cda7f6e63daf86f5550
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-01-01 20:33:52 +0100

    LondonBoroughs feat: moved plotting to a function

--- Data Viz/Python/Maps/LondonBoroughs/main.py
+++ Data Viz/Python/Maps/LondonBoroughs/main.py
@@ -27,39 +27,43 @@ merged = map_df.set_index('NAME').join(score.set_index('borough'))
 #set a variable that will call whatever column we want to visualise on the map
 variable = 'pop_density_per_hectare'
 
-#set the range for the chloropleth
-vmin, vmax = 120, 220
 
-#create figure and axes for MPL
-fig, ax = plt.subplots(1, figsize=(10,6))
+def plot_by(feature):
+    #set the range for the chloropleth
+    vmin, vmax = 120, 220
 
-#create map
-merged.plot(column=variable, cmap='Blues', linewidth=.8, ax=ax, edgecolor='.8')
+    #create figure and axes for MPL
+    fig, ax = plt.subplots(1, figsize=(10,6))
 
-#___prettify___
+    #create map
+    merged.plot(column=feature, cmap='Blues', linewidth=.8, ax=ax, edgecolor='.8')
 
-#axis remove
-ax.axis('off')
+    #___prettify___
 
-#set title
-ax.set_title('Preventable death rate in London',
-             fontdict={
-                 'fontsize' : '25',
-                 'fontweight' : '3'
-             })
+    #axis remove
+    ax.axis('off')
 
-#Annotation for data source
-ax.annotate('Source: London Datastore, 2014',
-            xy=(.1, .08),
-            xycoords='figure fraction',
-            horizontalalignment='left',
-            verticalalignment='top',
-            fontsize=10,
-            color='#555555')
+    #set title
+    ax.set_title('Preventable death rate in London',
+                 fontdict={
+                     'fontsize' : '25',
+                     'fontweight' : '3'
+                 })
 
-#colorbar as legend
-sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=vmin, vmax=vmax))
-sm._A = []
-cbar = fig.colorbar(sm)
+    #Annotation for data source
+    ax.annotate('Source: London Datastore, 2014',
+                xy=(.1, .08),
+                xycoords='figure fraction',
+                horizontalalignment='left',
+                verticalalignment='top',
+                fontsize=10,
+                color='#555555')
 
-plt.show()
+    #colorbar as legend
+    sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=vmin, vmax=vmax))
+    sm._A = []
+    cbar = fig.colorbar(sm)
+
+    plt.show()
+
+#plot_by(variable)
