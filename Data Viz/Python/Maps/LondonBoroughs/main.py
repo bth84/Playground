fb99d37efcf7bd130734c0e15d02055e3cdd4750
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-01-01 13:02:56 +0100

    bug: fixed data import and cleansing

--- Data Viz/Python/Maps/LondonBoroughs/main.py
+++ Data Viz/Python/Maps/LondonBoroughs/main.py
@@ -4,4 +4,20 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 
 map_df = gpd.read_file('London_Borough_Excluding_MHW.shp')
-df = pd.read_csv('london-borough-profiles.csv')
+df = pd.read_csv('london-borough-profiles.csv', header=0, encoding='ISO-8859-1')
+
+#column filtering, we are interested in
+df = df[['Area name',
+         'Happiness score 2011-14 (out of 10)',
+         'Anxiety score 2011-14 (out of 10)',
+         'Population density (per hectare) 2015',
+         'Mortality rate from causes considered preventable']]
+
+#rename, since ugly names
+score = df.rename(index=str, columns={
+    "Area name" : 'borough',
+    "Happiness score 2011-14 (out of 10)": "happiness",
+    "Anxiety score 2011-14 (out of 10)": "anxiety",
+    "Population density (per hectare) 2015": "pop_density_per_hectare",
+    "Mortality rate from causes considered preventable": 'mortality'
+})
