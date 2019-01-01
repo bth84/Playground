63bd4c8776a4836e97b083e91b4a7fda0a5361ea
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-01-01 13:15:34 +0100

    feat: added the plot itself
    Added the plot and some prettifications based on data for 'preventable death rate in London'

--- Data Viz/Python/Maps/LondonBoroughs/main.py
+++ Data Viz/Python/Maps/LondonBoroughs/main.py
@@ -21,3 +21,45 @@ score = df.rename(index=str, columns={
     "Population density (per hectare) 2015": "pop_density_per_hectare",
     "Mortality rate from causes considered preventable": 'mortality'
 })
+
+merged = map_df.set_index('NAME').join(score.set_index('borough'))
+
+#set a variable that will call whatever column we want to visualise on the map
+variable = 'pop_density_per_hectare'
+
+#set the range for the chloropleth
+vmin, vmax = 120, 220
+
+#create figure and axes for MPL
+fig, ax = plt.subplots(1, figsize=(10,6))
+
+#create map
+merged.plot(column=variable, cmap='Blues', linewidth=.8, ax=ax, edgecolor='.8')
+
+#___prettify___
+
+#axis remove
+ax.axis('off')
+
+#set title
+ax.set_title('Preventable death rate in London',
+             fontdict={
+                 'fontsize' : '25',
+                 'fontweight' : '3'
+             })
+
+#Annotation for data source
+ax.annotate('Source: London Datastore, 2014',
+            xy=(.1, .08),
+            xycoords='figure fraction',
+            horizontalalignment='left',
+            verticalalignment='top',
+            fontsize=10,
+            color='#555555')
+
+#colorbar as legend
+sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=vmin, vmax=vmax))
+sm._A = []
+cbar = fig.colorbar(sm)
+
+plt.show()
