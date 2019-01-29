1b544f3b54fddbb1c6154bdd815d99d03ab68e54
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-01-29 19:29:17 +0100

    feat: loading dataframe
    Loading data from 'data.csv' and first preprocessing

--- Data Viz/Python/Helpers/main.py
+++ Data Viz/Python/Helpers/main.py
@@ -15,3 +15,29 @@ color = sns.color_palette()
 
 pd.set_option('display.max_rows', 10000)
 pd.set_option('display.max_columns', 100)
+
+df = pd.read_csv('data.csv', encoding='ISO-8859-1') #latin-1 Encoding
+
+df.rename(inplace=True, index=str, columns={
+    'InvoiceNo': 'invoice_num',
+    'StockCode' : 'stock_code',
+    'Description' : 'description',
+    'Quantity' : 'quantity',
+    'InvoiceDate' : 'invoice_date',
+    'UnitPrice' : 'unit_price',
+    'CustomerID' : 'cust_id',
+    'Country' : 'country'
+})
+
+df['invoice_date'] = pd.to_datetime(df.invoice_date, format='%m/%d/%Y %H:%M')
+df['desciption'] = df.description.str.lower()
+df_new = df.dropna()
+df_new['cust_id'] = df_new['cust_id'].astype('int64')
+df_new = df_new[df_new.quantity > 0]
+df_new['amount_spent'] = df_new['quantity'] * df_new['unit_price']
+
+#rearange columns
+df_new = df_new[['invoice_num','invoice_date','stock_code','description','quantity','unit_price','amount_spent','cust_id','country']]
+
+df_new.insert(loc=2, column='year_month', value=df_new['invoice_date'].map(lambda x: 100*x.year + x.month))
+df_new.insert(loc=3, column='month', value=df_new.invoice_date.dt.month)
