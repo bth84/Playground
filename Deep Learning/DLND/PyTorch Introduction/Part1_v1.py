673a2edb5d5efed49c6ea0d45782c0851b1e2a19
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-01-31 22:15:24 +0100

    feat: torch-mnist example 'from scratch'

--- Deep Learning/DLND/PyTorch Introduction/Part1_v1.py
+++ Deep Learning/DLND/PyTorch Introduction/Part1_v1.py
@@ -57,7 +57,7 @@ import numpy as np
 
 a = np.random.rand(4,3)
 b = torch.from_numpy(a)
-print(b.numpy())
+#print(b.numpy())
 
 #Multiply PyTorch Tensor by 2, in place
 #print(b.mul_(2))
@@ -65,4 +65,52 @@ print(b.numpy())
 #Numpy array matches new values from tensor
 #print(a)
 
+
+
 #_____________________________________________________#
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((.5,  .5, .5), (.5, .5, .5))
+])
+
+trainset = datasets.MNIST('mnist_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+print(type(images))
+print(images.shape)
+print(labels.shape)
+
+plt.imshow(images[1].numpy().squeeze(), cmap='Greys_r');
+plt.show()
+
+def activation(x):
+    return 1 / (1 + torch.exp(-x))
+
+def softmax(x):
+    return torch.exp(x) / torch.sum(torch.exp(x), dim=1).view(-1,1)
+
+
+#flatten the input images
+inputs = images.view(images.shape[0], -1)
+
+#create parameters
+w1 = torch.randn(784,256)
+b1 = torch.randn(256)
+
+w2 = torch.randn(256,10)
+b2 = torch.randn(10)
+
+h = activation(torch.mm(inputs, w1) + b1)
+out = torch.mm(h, w2) + b2
+probabilities = softmax(out)
+
+#print(probabilities.shape)
+#print(probabilities.sum(dim=1))
+
+
