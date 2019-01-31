c982941085fa03f39174d352cbe948eb1ff57f64
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-01-31 22:44:21 +0100

    feat: ReLU + complexity
    introducing ReLU and further complexity. Still no training

--- Deep Learning/DLND/PyTorch Introduction/Part1_v1.py
+++ Deep Learning/DLND/PyTorch Introduction/Part1_v1.py
@@ -163,3 +163,24 @@ class Network(nn.Module):
 
 model = Network()
 #print(model)
+
+
+#_______ReLU + complexity_______#
+import torch.nn.functional as F
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden1 = nn.Linear(784, 128)
+        self.hidden2 = nn.Linear(128, 64)
+        self.output = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = F.relu(self.hidden1(x))
+        x = F.Relu(self.hidden2(x))
+        x = F.softmax(output)
+
+        return x
+
+model = Network()
+print(model)
