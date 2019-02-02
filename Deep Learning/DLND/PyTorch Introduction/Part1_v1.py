dacedd9c0823dfccbc278374f7a42016689d01a3
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-02-02 22:36:45 +0100

    feat: Fashion MNIST + test inferencing

--- Deep Learning/DLND/PyTorch Introduction/Part1_v1.py
+++ Deep Learning/DLND/PyTorch Introduction/Part1_v1.py
@@ -302,20 +302,104 @@ optimizer = optim.SGD(model.parameters(), lr=.003)
 criterion = nn.NLLLoss()
 
 epochs = 5
+# for e in range(epochs):
+#     running_loss = 0
+#     for images, labels in trainloader:
+#         #flatten images into 784 long vectors
+#         images = images.view(images.shape[0], -1)
+#
+#         #zero_grad!
+#         optimizer.zero_grad()
+#
+#         output = model.forward(images)
+#         loss = criterion(output, labels)
+#         loss.backward()
+#         optimizer.step()
+#
+#         running_loss += loss.item()
+#     else:
+#         print(f"Training loss: {running_loss/len(trainloader)}")
+
+
+# images, labels = next(iter(trainloader))
+# img = images[0].view(1, 784)
+#
+# with torch.no_grad():
+#     logits = model.forward(img)
+#
+# ps = F.softmax(logits, dim=1)
+#plt.imshow(img.view(28,28))
+#plt.show()
+#print(ps)
+
+#_____Fashion Mnist______#
+
+trainset = datasets.FashionMNIST('~/.pytorch/MNIST_data/', download=True, transform=transform, train=True,)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Download and load the test data
+testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784,256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+
+        self.dropout = nn.Dropout(p=.2)
+
+    def forward(self, x):
+        #make sure, tensor is flattened
+        x = x.view(x.shape[0], -1)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.log_softmax(self.fc4(x), dim=1)
+
+        return x
+
+model = Classifier()
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.parameters(), lr = .003)
+
+epochs = 30
+step = 0
+
+train_losses, test_losses = [], []
 for e in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
-        #flatten images into 784 long vectors
-        images = images.view(images.shape[0], -1)
+        logps = model(images)
+        loss = criterion(logps, labels)
 
-        #zero_grad!
         optimizer.zero_grad()
-
-        output = model.forward(images)
-        loss = criterion(output, labels)
         loss.backward()
         optimizer.step()
 
         running_loss += loss.item()
     else:
-        print(f"Training loss: {running_loss/len(trainloader)}")
+        test_loss = 0
+        accuracy = 0
+        print(f"Training loss: {running_loss}")
+
+        with torch.no_grad():
+            for images, labels in testloader:
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+        train_losses.append(test_loss/len(trainloader))
+        test_losses.append(test_loss/len(testloader))
+
+        print("Epoch: {}/{}..".format(e+1, epochs),
+              "Training Loss: {:.3f}..".format(running_loss/len(trainloader)),
+              "Test loss: {:.3f}..".format(test_loss/len(testloader)),
+              "Test accuracy: {:.3f}".format(accuracy/len(testloader)))
