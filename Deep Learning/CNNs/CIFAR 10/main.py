723c40d450d07174fd2b150724d18dfbb5ea9d3c
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-02-05 23:38:35 +0100

    feat: validating the model against test holdout
    Including accuracys for each class!

--- Deep Learning/CNNs/CIFAR 10/main.py
+++ Deep Learning/CNNs/CIFAR 10/main.py
@@ -1,64 +1,63 @@
+from pathlib import Path
+
 import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-
 from torch import nn
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets
 from torchvision import transforms
-
-from pathlib import Path
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        #convolutional layer, sees 32x32x3 image tensor
-        #has 3 input channels (R,G,B Colors), goes for 16 filters and
-        #has a kernel size of 3. To avoid shrinking of the output layers
-        #padding is set to 1, since kernel size is 3
-        self.conv1 = nn.Conv2d(3,16,3, padding=1)
-        self.conv2 = nn.Conv2d(16,32,3, padding=1)
+        # convolutional layer, sees 32x32x3 image tensor
+        # has 3 input channels (R,G,B Colors), goes for 16 filters and
+        # has a kernel size of 3. To avoid shrinking of the output layers
+        # padding is set to 1, since kernel size is 3
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
-        self.pool = nn.MaxPool2d(2,2)
+        self.pool = nn.MaxPool2d(2, 2)
 
-        #final classifier
-        #linear layer #1:  64 * 4 * 4, 500
-        self.fc1 = nn.Linear(64*4*4, 500)
-        #output to 10 different classes
-        self.fc2 = nn.Linear(500,10)
+        # final classifier
+        # linear layer #1:  64 * 4 * 4, 500
+        self.fc1 = nn.Linear(64 * 4 * 4, 500)
+        # output to 10 different classes
+        self.fc2 = nn.Linear(500, 10)
 
-        #Regularization
+        # Regularization
         self.dropout = nn.Dropout(.25)
 
-    def forward(self,x):
-
-        #Sequence of Convolutionals
+    def forward(self, x):
+        # Sequence of Convolutionals
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
 
-        #flatten image input
+        # flatten image input
         x = x.view(-1, 64 * 4 * 4)
 
-        #add dropout
+        # add dropout
         x = self.dropout(x)
 
-        #classifier
-        #both classifier layers with relu activation and first one with dropout
+        # classifier
+        # both classifier layers with relu activation and first one with dropout
         x = self.dropout(F.relu(self.fc1(x)))
         x = self.fc2(x)
 
         return x
+
 
 batch_size = 20
 valid_size = .2
 
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((.5,.5,.5),(.5,.5,.5))
+    transforms.Normalize((.5, .5, .5), (.5, .5, .5))
 ])
 
 # fetch and store CIFAR 10
@@ -67,28 +66,26 @@ train_data = datasets.CIFAR10('data', train=True,
 test_data = datasets.CIFAR10('data', train=False,
                              download=True, transform=transform)
 
-
-#obtain training indices that will be used for validation
+# obtain training indices that will be used for validation
 num_train = len(train_data)
 indices = list(range(num_train))
 np.random.shuffle(indices)
 split = int(np.floor(valid_size * num_train))
 train_idx, valid_idx = indices[split:], indices[:split]
 
-#samplers for obtaining training and validation batches
+# samplers for obtaining training and validation batches
 train_sampler = SubsetRandomSampler(train_idx)
 test_sampler = SubsetRandomSampler(valid_idx)
 
-#prepare data loaders (combine dataset and sampler)
+# prepare data loaders (combine dataset and sampler)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=train_sampler)
 valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=test_sampler)
 
-#image classes
+# image classes
 classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck']
 
-
-#____Modelling____
+# ____Modelling____
 model = Net()
 print(model)
 
@@ -97,7 +94,7 @@ optimizer = optim.SGD(model.parameters(), lr=.01)
 
 n_epochs = 30
 
-#Track change in validation loss
+# Track change in validation loss
 valid_loss_min = np.Inf
 
 model_file = Path("model_cifar.pt")
@@ -156,3 +153,51 @@ if not model_file.is_file():
 
 # after the epochs have been computed, load the best validated model
 model.load_state_dict(torch.load('model_cifar.pt'))
+
+######################################
+# run the model against test holdout #
+######################################
+
+# track test loss
+test_loss = 0.0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+model.eval()
+# iterate over test data
+for data, target in test_loader:
+    # move tensors to GPU if CUDA is available
+    if train_on_gpu:
+        data, target = data.cuda(), target.cuda()
+    # forward pass: compute predicted outputs by passing inputs to the model
+    output = model(data)
+    # calculate the batch loss
+    loss = criterion(output, target)
+    # update test loss
+    test_loss += loss.item() * data.size(0)
+    # convert output probabilities to predicted class
+    _, pred = torch.max(output, 1)
+    # compare predictions to true label
+    correct_tensor = pred.eq(target.data.view_as(pred))
+    correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+    # calculate test accuracy for each object class
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# average test loss
+test_loss = test_loss / len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            classes[i], 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
