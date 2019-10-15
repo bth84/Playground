c542bf0ba76ce2b481350f2b449c48d1159c8b04
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-10-15 20:18:53 +0200

    feat: added transform pipeline
    Different transform.Compose Pipelines for 'train' and 'validation' each

--- Deep Learning/CNNs/AlienVSPredator/AvP_main.py
+++ Deep Learning/CNNs/AlienVSPredator/AvP_main.py
@@ -1,2 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import torch
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
+
+normalize = transforms.Normalize(mean=[.485, .456, .406], std=[.229, .224, .225])
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.RandomAffine(0, shear=10, scale=(.8, 1.2)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ]),
+    'validation': transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.normalize
+    ])
+}
+
+
 if __name__ = '__main__':
     pass
