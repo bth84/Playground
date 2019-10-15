3c9fd7866d9d477f430c1a86850151d7b9d2a2b4
Benjamin Thorand <thorand.benjamin@gmail.com>
2019-10-15 23:58:38 +0200

    feat: new (human) images

--- Deep Learning/CNNs/AlienVSPredator/AvP_main.py
+++ Deep Learning/CNNs/AlienVSPredator/AvP_main.py
@@ -8,6 +8,9 @@ import torch.nn as nn
 from torch.nn import functional as F
 import torch.optim as optim
 
+
+
+
 # ---------------
 # data generators
 # ---------------
@@ -44,10 +47,12 @@ data_loaders = {
                                          num_workers=4)
 }
 
+
+
+
 # ------------------
 # create the network
 # ------------------
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = models.resnet50(pretrained=True).to(device)
@@ -63,5 +68,90 @@ model.fc = nn.Sequential(
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(params=model.fc.parameters())
 
+
+# ---------------
+# train the model
+# ---------------
+def train_model(model, criterion, optimizer, num_epochs=3):
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+        print('-' * 10)
+
+        for phase in ['train', 'validation']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in data_loaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.detach() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(image_datasets[phase])
+            epoch_acc = running_corrects.float() / len(image_datasets[phase])
+
+            print('{} loss: {:.4f}, acc: {:.4f}'.format(phase,
+                                                        epoch_loss.item(),
+                                                        epoch_acc.item()))
+    return model
+
+
+
 if __name__ == '__main__':
-    pass
+    # model_trained = train_model(model, criterion, optimizer, num_epochs=5)
+
+    # -----------------------
+    # save and load the model
+    # -----------------------
+    # torch.save(model_trained.state_dict(), 'models/pytorch/weights.h5')
+    model = models.resnet50(pretrained=False).to(device)
+    model.fc = nn.Sequential(
+        nn.Linear(2048, 128),
+        nn.ReLU(inplace=True),
+        nn.Linear(128, 2)).to(device)
+    model.load_state_dict(torch.load('models/pytorch/weights.h5'))
+
+    # ---------------------------------
+    # predictions on sample test images
+    # ---------------------------------
+
+    # validation_img_paths = ["data/validation/alien/24.jpg",
+    #                         "data/validation/alien/27.jpg",
+    #                         "data/validation/predator/33.jpg"]
+
+    humans = ['data/humans/adnan.jpg',
+              'data/humans/ben.jpg',
+              'data/humans/ela.jpg']
+
+    #img_list = [Image.open(img_path) for img_path in validation_img_paths]
+    img_list = [Image.open(img_path) for img_path in humans]
+    validation_batch = torch.stack([data_transforms['validation'](img).to(device)
+                                    for img in img_list])
+
+    pred_logits_tensor = model(validation_batch)
+    pred_probs = F.softmax(pred_logits_tensor, dim=1).cpu().data.numpy()
+
+    fig, axs = plt.subplots(1, len(img_list), figsize=(20, 5))
+    for i, img in enumerate(img_list):
+        ax = axs[i]
+        ax.axis('off')
+        ax.set_title("{:.0f}% Alien, {:.0f}% Predator".format(100 * pred_probs[i, 0],
+                                                              100 * pred_probs[i, 1]))
+        ax.imshow(img)
+
+    plt.show()
